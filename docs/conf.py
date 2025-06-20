435cf85b5119b544f6171c0961707ab1db71c658
SigmaCat <sigmacat_12@outlook.com>
2025-06-20 15:57:16 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,3 @@
 project = 'GPTScript'
 author = 'SigmaCat'
 release = '0.1.7'
-
-master_doc = 'index'
-html_theme = 'alabaster'
-html_experimental_html5_writer = True
-
-extensions = []
-
-exclude_patterns = []
